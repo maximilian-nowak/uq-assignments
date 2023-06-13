4d202c5bc6dadf06a66216e99583c1333c09eba6
Maximilian Nowak <maximilian.nowak@hm.edu>
2023-06-13 14:06:14 +0200

    add experimental notebook for assignment 3. last two tasks still pending

---
+++ src/assignment3/sobol.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def create_sobol_matrices(A, B):
+    """ Generates sample matrices by switching the ith column of matrix A and
+        the ith column of matrix B.
+    """
+    # ensure correct input shapes
+    if A.shape != B.shape:
+        raise Exception("Error: matrices A and B must be of same shape.")
+    
+    # construct list of matrices A with length of columns
+    N, n = A.shape
+    samples_AB = np.zeros((n, N, n))
+    
+    # create samples by switching ith column of A by ith column of B
+    for i in range(0, samples_AB.shape[0]):
+        samples_AB[i,:,:] = A
+        samples_AB[i,:,i] = B[:,i]
+        
+    return samples_AB
+
+def compute_total_indices(Y_A, Y_AB):
+    """computes the total-effect sobol indices using Jansen's estimator.
+
+    Args:
+        f_A (np.array((N,))): model function
+        f_AB (np.array((n, N))): Model function with swapped columns.
+
+    Returns:
+        np.array((N,)): array of total Sobol indices for each input variable.#
+    """
+    N = Y_A.shape[0]
+    VarY = np.var(Y_A)
+    Ti = (1/(2*N) * np.sum((Y_A - Y_AB)**2, axis=1)) / VarY
+    return Ti
