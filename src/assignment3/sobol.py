da6f99f155b2ff3a9b7490d83f5c888083e3443e
Maximilian Nowak <maximilian.nowak@hm.edu>
2023-06-14 10:44:29 +0200

    code clean up in sobol scripts

--- src/assignment3/sobol.py
+++ src/assignment3/sobol.py
@@ -10,16 +10,16 @@ def create_sobol_matrices(A, B):
     
     # construct list of matrices A with length of columns
     N, n = A.shape
-    samples_AB = np.zeros((n, N, n))
+    samples_Ab = np.zeros((n, N, n))
     
     # create samples by switching ith column of A by ith column of B
-    for i in range(0, samples_AB.shape[0]):
-        samples_AB[i,:,:] = A
-        samples_AB[i,:,i] = B[:,i]
+    for i in range(0, samples_Ab.shape[0]):
+        samples_Ab[i,:,:] = A
+        samples_Ab[i,:,i] = B[:,i]
         
-    return samples_AB
+    return samples_Ab
 
-def compute_total_indices(f_A, f_AB):
+def compute_total_indices(f_A, f_Ab):
     """computes the total-effect sobol indices using Jansen's estimator.
 
     Args:
@@ -27,9 +27,9 @@ def compute_total_indices(f_A, f_AB):
         f_AB (np.array((n, N))): Model function with swapped columns.
 
     Returns:
-        np.array((N,)): array of total Sobol indices for each input variable.#
+        np.array((N,)): array of total Sobol indices for each input variable.
     """
     N = f_A.shape[0]
-    Var_f_A = np.var(f_A)
-    Ti = (1/(2*N) * np.sum((f_A - f_AB)**2, axis=1)) / Var_f_A
+    var_f_A = np.var(f_A)
+    Ti = (1/(2*N) * np.sum((f_A - f_Ab)**2, axis=1)) / var_f_A
     return Ti
