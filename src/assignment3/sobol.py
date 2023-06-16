ecf68958fa785b73a719ee52a38dde20cbce54cf
Maximilian Nowak <maximilian.nowak@hm.edu>
2023-06-16 20:26:14 +0200

    move computation of QoIs in seperate function

--- src/assignment3/sobol.py
+++ src/assignment3/sobol.py
@@ -1,8 +1,12 @@
 import numpy as np
+from assignment1.seir import seirc_model, G1, G2
 
 def create_sobol_matrices(A, B):
     """ Generates sample matrices by switching the ith column of matrix A and
         the ith column of matrix B.
+        
+    Returns:
+        np.array((n,N,n)): list of matrices with length of number of columns of A.
     """
     # ensure correct input shapes
     if A.shape != B.shape:
@@ -19,6 +23,7 @@ def create_sobol_matrices(A, B):
         
     return samples_Ab
 
+
 def compute_total_indices(f_A, f_Ab):
     """computes the total-effect sobol indices using Jansen's estimator.
 
@@ -33,3 +38,26 @@ def compute_total_indices(f_A, f_Ab):
     var_f_A = np.var(f_A)
     Ti = (1/(2*N) * np.sum((f_A - f_Ab)**2, axis=1)) / var_f_A
     return Ti
+
+def compute_QoIs_from_samples(X0, args):
+    """Runs the seirc model by sampling parameters from X0 and 
+       determines quantities of interest (QoI) by applying G1 and G2.
+
+    Args:
+        X0 (np.array(N, n)): matrix containing parameter combinations.
+
+    Returns:
+        np.array((2, N)): G1 and G2
+    """
+    (N, S0, E0, i0, R0, C0), t = args
+    
+    g1_results, g2_results = [], []
+    # run model with sampled parameters and apply QoIs
+    for beta, alpha, gamma, i0 in X0:
+        S0 = N - i0
+        y0 = [S0, E0, i0, R0, C0]  # update initial conditions with i0
+        sol = seirc_model((beta, alpha, gamma, N)).solve(t, y0)
+        g1_results.append(G1(sol[:, 4]))
+        g2_results.append(G2(sol[:, 2], t))
+    
+    return np.array([g1_results, g2_results])
